5329b07eb98187172f300c83921c0ca8d7388cb6
willfeliz1 <william_mfelizardo@hotmail.com>
2021-01-06 18:15:49 -0300

    feat: database connection

---
+++ src/utils/connection.ts
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb+srv://feliz:feliz@cluster0.btnxp.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
