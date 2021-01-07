c695d4532006dce599f75713f47d959242eb0cc8
willfeliz1 <william_mfelizardo@hotmail.com>
2021-01-07 02:03:59 -0300

    feat: video and Category resolver in Server

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,11 @@ import './utils/connection';
 import { buildSchema } from 'type-graphql';
 import { ApolloServer } from "apollo-server";
 import CategoryResolver from "./graphql/category/CategoryResolver";
+import VideoResolver from "./graphql/video/VideoResolver";
 
 async function bootstrap() {
   const schema = await buildSchema({
-    resolvers: [CategoryResolver],
+    resolvers: [CategoryResolver, VideoResolver],
   });
 
   const server = new ApolloServer({ schema });
