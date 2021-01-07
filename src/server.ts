f57840ab4c78990913bb79e8db33ecd10ddf110d
willfeliz1 <william_mfelizardo@hotmail.com>
2021-01-07 00:27:25 -0300

    feat: Server

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import './utils/connection';
+import { buildSchema } from 'type-graphql';
+import { ApolloServer } from "apollo-server";
+import CategoryResolver from "./graphql/category/CategoryResolver";
+
+async function bootstrap() {
+  const schema = await buildSchema({
+    resolvers: [CategoryResolver],
+  });
+
+  const server = new ApolloServer({ schema });
+
+  server.listen({ port: 4100 }, () => console.log('Running'));
+
+}
+
+bootstrap();
