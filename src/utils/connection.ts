528e9ef1f0bce4159fb30d5343ac5560e25fc2f7
willfeliz1 <william_mfelizardo@hotmail.com>
2021-01-08 18:06:01 -0300

    fix: uri

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb+srv://feliz:feliz@cluster0.btnxp.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect('uri',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
